e98c3c0862c8db41cf750caca8cbc469be74aafc
nardos-tsige <tsigeferejanardos@gmail.com>
2025-12-05 00:51:19 +0300

    Implement selection sort algorithm

---
+++ Selection Sort.py
@@ -0,0 +1,10 @@
+class Solution: 
+    def selectionSort(self, arr):
+      #code here
+        
+        for i in range(len(arr) - 1):
+            for j in range(len(arr) - 1 - i):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                    
+        return arr
