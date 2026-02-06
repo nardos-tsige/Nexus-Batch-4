7694942fd343826244fda5645f9098a156574247
nardos-tsige <tsigeferejanardos@gmail.com>
2026-02-06 14:56:39 +0300

    Create Largest Number.py

---
+++ Largest Number.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        
+        str_nums = [str(num) for num in nums]
+    
+        n = len(str_nums)
+        for i in range(n):
+            for j in range(i+1, n):
+
+                if str_nums[i] + str_nums[j] < str_nums[j] + str_nums[i]:
+                 
+                    str_nums[i], str_nums[j] = str_nums[j], str_nums[i]
+        
+    
+        result = ''.join(str_nums)
+        return result if result[0] != '0' else '0'
