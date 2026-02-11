683c17fa94e6ce37e8ad8f24472d6cd3688ac592
nardos-tsige <tsigeferejanardos@gmail.com>
2026-02-11 10:13:02 +0300

    Add nextGreaterElement method in Solution class
    Implement next greater element algorithm for two lists.

---
+++ next-greater-element-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        next_greater = {}
+        stack = []
+        for num in nums2:
+            while stack and num > stack[-1]:
+                smaller = stack.pop()
+                next_greater[smaller] = num
+            stack.append(num)
+        while stack:
+            next_greater[stack.pop()] = -1
+        result = []
+        for num in nums1:
+            result.append(next_greater[num])
+        
+        return result
