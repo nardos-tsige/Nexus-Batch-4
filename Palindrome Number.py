6300e1d8336bc8bd37daa1ff9e3873d4e1a2064d
nardos-tsige <tsigeferejanardos@gmail.com>
2026-02-17 19:03:10 +0300

    Update Palindrome Number.py

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -1,12 +1,7 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        rem, ana = 0, x
-        if ana < 0:
-            return False
-        while ana > 0:
-            s = ana % 10
-            rem = (rem * 10) + s
-            ana = ana // 10
-        if rem == x:
+        nummy = str(x)
+
+        if (str(x) == nummy[::-1]):
             return True
         return False
