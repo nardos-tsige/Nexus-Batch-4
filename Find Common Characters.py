29f9c028fe4076ab0e1bb352438e1d12054dff5e
nardos-tsige <tsigeferejanardos@gmail.com>
2026-02-19 10:02:17 +0300

    Update Find Common Characters.py

--- Find Common Characters.py
+++ Find Common Characters.py
@@ -1,12 +1,19 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        k = []
-        for ch in set(words[0]): 
-            min_count = float('inf')
+       
+        first_word = words[0]
+        result = []
+        for char in set(first_word):
+            
+            min_occurrences = first_word.count(char)
+            for word in words[1:]:
+                current_count = word.count(char)
+               
+                if current_count == 0:
+                    min_occurrences = 0
+                    break
+                min_occurrences = min(min_occurrences, current_count)
 
-            for word in words:
-                min_count = min(min_count, word.count(ch))
-
-            k.extend([ch] * min_count)
-
-        return k
+            result.extend([char] * min_occurrences)
+        
+        return result
